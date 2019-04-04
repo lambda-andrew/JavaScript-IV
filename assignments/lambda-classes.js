10ccd7d55d0527af9025409f7a43b4423afdd0ca
Oliver-Strange <preston.burton@gmail.com>
2019-04-04 13:04:40 -0700

    added all the classes to lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,56 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+class Instructor extends Person{
+    constructor(instAttributes){
+        super(instAttributes);
+        this.specialty = instAttributes.specialty;
+        this.favLanguage = instAttributes.favLanguage;
+        this.catchPhrase = instAttributes.catchPhrase;
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}!`);
+    }
+    grade(student,subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
+class Student extends Person {
+    constructor(studentAttributes){
+        super(studentAttributes);
+        this.previousBackground = studentAttributes.previousBackground;
+        this.className = studentAttributes.className;
+        this.favSubjects = studentAttributes.favSubjects;
+    }
+    listSubjects(){
+        console.log(studentAttributes.favSubjects);
+    }
+    prAssignment(subject){
+        console.log(`${student.name} has submitted a PR for ${subject}.`);
+    }
+    sprintChallenge(subject){
+        console.log(`${student.name} has begun sprint challenge on ${subject}.`);
+    }
+}
+class ProjectManager extends Instructor{
+    constructor(pmAttributes){
+        super(pmAttributes);
+        this.gradClassName = pmAttributes.gradClassName;
+        this.favInstructor = pmAttributes.favInstructor;
+    }
+    standUp(channel){
+        console.log(`${name} announces to ${channel}, @${channel} standy times!`);
+    }
+    debugsCode(student,subject){
+        console.log(`${name} debugs ${student.name}'s code on ${subject}.`);
+    }
+}
