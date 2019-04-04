7dcdbbe398a5b0f108282d703fa13cc7fdea25af
Andy Bettisworth <andy@accreu.com>
2019-04-04 15:00:25 -0500

    doc: add task

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+
+// Create 2 - 3 objects for each class and test their attributes:
+
+// const fred = new Instructor({
+//   name: 'Fred',
+//   location: 'Bedrock',
+//   age: 37,
+//   gender: 'male',
+//   favLanguage: 'JavaScript',
+//   specialty: 'Front-end',
+//   catchPhrase: `Don't forget the homies`
+// });
