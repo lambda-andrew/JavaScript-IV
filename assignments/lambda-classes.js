30b9595107adcebc9ade688cb91ad5aefca56846
BrennanHolm <brennan.mholmich@gmail.com>
2019-04-11 18:49:00 -0500

    Add objects for Person class
    Adds 'firstGuess', 'secondGuest', and 'thirdGuest'. Also adds return
    to all class method.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,9 +8,9 @@ class Person {
     this.gender = personProps.gender;
   }
   speak() {
-    `Hello, my name is ${this.name}, I am from ${this.location}`
+    return `Hello, my name is ${this.name}, I am from ${this.location}`;
   }
-}
+};
 
 class Instructor extends Person {
   constructor(instProps) {
@@ -20,43 +20,69 @@ class Instructor extends Person {
     this.catchPhrase = instProps.catchPhrase;
   }
   demo(subject) {
-    `Today we are learning about ${subject}`
+    return `Today we are learning about ${subject}`;
   }
-  grade(subject) {
-    `${student.name} receives a perfect score on ${subject}`
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
   }
-}
+};
 
 class Student extends Person {
   constructor(studentProps) {
-    super(studentProps) {
-      this.previousBackground = studentProps.previousBackground;
-      this.className = studentProps.className;
-      this.favSubjects = studentProps.favSubjects;
-    }
+    super(studentProps);
+    this.previousBackground = studentProps.previousBackground;
+    this.className = studentProps.className;
+    this.favSubjects = studentProps.favSubjects;
   }
   listsSubjects() {
-    `Favorite subjects are: ${this.favSubject}`
+    return `Favorite subjects are: ${this.favSubjects[0]}, ${this.favSubjects[1]}, and ${this.favSubjects[2]}.`;
   }
   PRAssignment(subject) {
-    `${this.name} has submitted a PR for ${subject}`
+    return `${this.name} has submitted a PR for ${subject}`;
   }
   sprintChallenge(subject) {
-    `${this.name} has begun sprint challenge on ${subject}`
+    return `${this.name} has begun sprint challenge on ${subject}`;
   }
-}
+};
 
 class ProjectManagers extends Instructor {
   constructor(pmProps) {
-    super(pmProps) {
-      this.gradClassName = pmProps.gradClassName;
-      this.favInstructor = pmProps.favInstructor;
-    }
+    super(pmProps);
+    this.gradClassName = pmProps.gradClassName;
+    this.favInstructor = pmProps.favInstructor;
   }
   standUp(channel) {
-    `${this.name} announces to ${channel}, @channel standy times!​​​​​`
+    return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
   }
   debugsCode(student, subject) {
-    `${this.name} debugs ${student.name}'s code on ${subject}``
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
   }
-}
+};
+
+// == Character Roster ==
+
+// == Person ==
+const firstGuest = new Person({
+  name: 'Arianna',
+  age: 30,
+  location: 'Cathedral Ward',
+  gender: 'female'
+});
+
+const secondGuest = new Person({
+  name: 'Gilbert',
+  age: 40,
+  location: 'Central Yarnham',
+  gender: 'male'
+});
+
+const thirdGuest = new Person({
+  name: 'Patches the Spider',
+  age: 10000,
+  location: 'the Lecture Building',
+  gender: 'male'
+});
+
+/* == Testing == */
+// console.log(firstGuest);
+// console.log(thirdGuest.speak());
