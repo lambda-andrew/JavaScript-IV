c216c540db2210a633a295dd1aec07ea2deb0fa8
BrennanHolm <brennan.mholmich@gmail.com>
2019-04-11 16:40:36 -0500

    Add base Person class to lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(personProps) {
+    this.name = personProps.name;
+    this.age = personProps.age;
+    this.location = personProps.location;
+    this.gender = personProps.gender;
+  }
+  speak() {
+    `Hello, my name is ${this.name}, I am from ${this.location}`
+  }
+}
