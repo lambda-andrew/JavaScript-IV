6654c013491a62f9c3ba6756c65a6cd5c31ea4e1
Eric Nunn <lowlyherotech@gmail.com>
2019-04-11 14:59:47 -0400

    Started constructing classes in lambda-classes.js. Person and Instructor function complete

---
+++ assignments/lambda-classes.js
@@ -0,0 +1,24 @@
+class Person {
+    constructor(props) {
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.location;
+        this.gender = props.gender;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+}
+
+class Instructor extends Person {
+    constructor(props){
+        super(props);
+        this.specialty = props.specialty;
+        this.favLanguage = props.favLanguage;
+        this.catchPhrase = props.catchPhrase;
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`);
+    }
+}
+
