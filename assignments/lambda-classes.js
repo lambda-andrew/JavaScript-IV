6564d8c5494cb1c1207b51809a458eba07bdab07
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-04-11 14:58:22 -0700

    JS2Flex

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(prop){
+        this.name = prop.age;
+        this.age = prop.age;
+        this.location = prop.location;
+        this.gender = prop.location;
+    };
+};
+
+//
