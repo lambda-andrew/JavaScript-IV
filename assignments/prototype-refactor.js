523310123a0cd926f581d130eee6414bd62ed358
Eric Nunn <lowlyherotech@gmail.com>
2019-04-11 11:30:28 -0400

    Refactored constructor functions into ES6 classes

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,13 +1,12 @@
-/* 
+/*
 
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
 /*
   === GameObject ===
   * createdAt
@@ -15,36 +14,34 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
-
-const GameObject = function(properties){
-    this.createdAt = properties.createdAt;
-    this.name = properties.name;
-    this.dimensions = properties.dimensions;
-  };
+class GameObject {
+    constructor(properties) {
+        this.createdAt = properties.createdAt;
+        this.name = properties.name;
+        this.dimensions = properties.dimensions;
+    }
+    destroy() {
+        return `${this.name} was removed from the game.`;
+    }
+}
   
-  GameObject.prototype.destroy = function(){
-    return `${this.name} was removed from the game.`;
-  }
-  
-  /*
+/*
     === CharacterStats ===
     * healthPoints
     * takeDamage() // prototype method -> returns the string '<object name> took damage.'
     * should inherit destroy() from GameObject's prototype
-  */
-  
-  function CharacterStats(charProps){
-    GameObject.call(this, charProps);
-    this.healthPoints = charProps.healthPoints;
-  }
-  
-  CharacterStats.prototype = Object.create(GameObject.prototype);
-  
-  CharacterStats.prototype.takeDamage = function(){
-    return `${this.name} took damage.`;
-  }
-  
-  /*
+*/
+class CharacterStats extends GameObject {
+    constructor(charProps) {
+        super(charProps);
+        this.healthPoints = charProps.healthPoints;
+    }
+    takeDamage() {
+        return `${this.name} took damage.`;
+    }
+}
+    
+/*
     === Humanoid (Having an appearance or character resembling that of a human.) ===
     * team
     * weapons
@@ -52,31 +49,27 @@ const GameObject = function(properties){
     * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
     * should inherit destroy() from GameObject through CharacterStats
     * should inherit takeDamage() from CharacterStats
-  */
+*/
+
+class Humanoid extends CharacterStats {
+    constructor(humanProps) {
+        super(humanProps);
+        this.team = humanProps.team;
+        this.weapons = humanProps.weapons;
+        this.language = humanProps.language;
+    }
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}.`;
+    }
+}
    
-  function Humanoid(humanProps){
-    GameObject.call(this, humanProps);
-    CharacterStats.call(this, humanProps)
-    this.team = humanProps.team;
-    this.weapons = humanProps.weapons;
-    this.language = humanProps.language;
-  }
-  
-  Humanoid.prototype = Object.create(CharacterStats.prototype);
-  
-  Humanoid.prototype.greet = function(){
-    return `${this.name} offers a greeting in ${this.language}.`;
-  }
-  
-  
-  
-  /*
+/*
     * Inheritance chain: GameObject -> CharacterStats -> Humanoid
     * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
     * Instances of CharacterStats should have all of the same properties as GameObject.
-  */
+*/
   
-  // Test you work by un-commenting these 3 objects and the list of console logs below:
+// Test you work by un-commenting these 3 objects and the list of console logs below:
   
   
     const mage = new Humanoid({
