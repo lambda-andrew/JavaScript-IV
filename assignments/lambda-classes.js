cd96ca8d8147a3f095b3912b2264dc86e0f5938c
BrennanHolm <brennan.mholmich@gmail.com>
2019-04-11 16:58:29 -0500

    Add Instructor class to lambda-class.js
    Instructor extends from Person. It uses the same props as Person.
    Props added are, 'specialty' 'favLanguage' and 'catchPhrase'.
    Methods added are, demo() and grade().

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,18 @@ class Person {
     `Hello, my name is ${this.name}, I am from ${this.location}`
   }
 }
+
+class Instructor extends Person {
+  constructor(instProps) {
+    super(instProps);
+    this.specialty = instProps.gender;
+    this.favLanguage = instProps.gender;
+    this.catchPhrase = instProps.gender;
+  }
+  demo() {
+    `Today we are learning about ${subject}`
+  }
+  grade() {
+    `${student.name} receives a perfect score on ${subject}`
+  }
+}
