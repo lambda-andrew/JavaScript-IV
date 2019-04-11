164cc5665027083083c909d793fbe7f676219cf4
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-04-11 16:46:39 -0700

    JS4Flex

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -47,11 +47,11 @@ class Student extends Person{
     };
 
     PRAssignment(subject) {
-        return `${student.name} has submitted a PR for ${subject}`
+        return `${this.name} has submitted a PR for ${subject}`
     };
 
     sprintChallenge(subject) {
-        return `${student.name} has begun sprint challenge on ${subject}` 
+        return `${this.name} has begun sprint challenge on ${subject}` 
     };
 };
 
